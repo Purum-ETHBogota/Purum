bcab39eba4f215f36261fed8492b0f23991c3d84
wildanvin <danvin.jim@gmail.com>
2022-10-08 23:02:41 -0500

    verify contracts, update abi & adresses

--- hardhat-purum/hardhat.config.js
+++ hardhat-purum/hardhat.config.js
@@ -98,7 +98,7 @@ module.exports = {
     apiKey: {
       rinkeby: ETHERSCAN_API_KEY,
       kovan: ETHERSCAN_API_KEY,
-      polygon: POLYGONSCAN_API_KEY,
+      polygonMumbai: POLYGONSCAN_API_KEY,
     },
   },
   gasReporter: {
